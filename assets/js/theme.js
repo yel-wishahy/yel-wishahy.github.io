3bc3aaf5339070148b3ac5885cfd0f891c14d0b7
yel-wishahy <yel.wishahy@gmail.com>
2022-01-17 00:42:56 -0800

    major content additon, some contact format changes and added contact form

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,17 +4,9 @@ document.querySelectorAll('.datepicker').forEach(function(field) {
 	});
 });
 
-$(document).ready(function() {
-
-  if(window.location.href.indexOf('#modalRobot') != -1) {
-    $('#modalRobot').modal('show');
-  }
-
-});
-
-$(document).ready(function() {  
+document.ready(function() {  
   var modals = ['#modalRobot', '#anotherModal'];
   if (window.location.hash && ~modals.indexOf(window.location.hash)) {
      $(window.location.hash).modal();
   }
-})
+});
